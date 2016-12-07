6384868eef192aedb03aacd69cb67d846f631e69
Jon Sangster <jon@ertt.ca>
2016-12-07 13:47:29 -0500

    Attempt to fix staging server DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120025232) do
+ActiveRecord::Schema.define(version: 20161130090127) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -70,6 +70,30 @@ ActiveRecord::Schema.define(version: 20161120025232) do
 
   add_index "cost_adjustments", ["person_id"], name: "index_cost_adjustments_on_person_id"
 
+  create_table "cost_code_charges", force: :cascade do |t|
+    t.integer  "cost_code_id"
+    t.integer  "max_days",           default: 0
+    t.integer  "adult_cents",        default: 0
+    t.integer  "teen_cents",         default: 0
+    t.integer  "child_cents",        default: 0
+    t.integer  "infant_cents",       default: 0
+    t.integer  "child_meal_cents",   default: 0
+    t.integer  "single_delta_cents", default: 0
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  add_index "cost_code_charges", ["cost_code_id", "max_days"], name: "index_cost_code_charges_on_cost_code_id_and_max_days", unique: true
+  add_index "cost_code_charges", ["cost_code_id"], name: "index_cost_code_charges_on_cost_code_id"
+
+  create_table "cost_codes", force: :cascade do |t|
+    t.string   "name",                    null: false
+    t.text     "description"
+    t.integer  "min_days",    default: 1, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
   create_table "course_attendances", force: :cascade do |t|
     t.integer  "course_id"
     t.integer  "attendee_id"
@@ -116,11 +140,15 @@ ActiveRecord::Schema.define(version: 20161120025232) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "housing_type",           default: 0, null: false
+    t.integer  "cost_code_id"
+    t.string   "cafeteria"
   end
 
+  add_index "housing_facilities", ["cost_code_id"], name: "index_housing_facilities_on_cost_code_id"
+
   create_table "housing_preferences", force: :cascade do |t|
-    t.integer  "family_id",      null: false
-    t.integer  "housing_type",   null: false
+    t.integer  "family_id",                   null: false
+    t.integer  "housing_type",                null: false
     t.string   "location1"
     t.string   "location2"
     t.string   "location3"
@@ -129,8 +157,11 @@ ActiveRecord::Schema.define(version: 20161120025232) do
     t.date     "confirmed_at"
     t.integer  "children_count"
     t.integer  "bedrooms_count"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "single_room"
+    t.string   "other_family"
+    t.boolean  "accepts_non_air_conditioned"
   end
 
   create_table "housing_units", force: :cascade do |t|
@@ -201,6 +232,22 @@ ActiveRecord::Schema.define(version: 20161120025232) do
   add_index "sessions", ["cas_ticket"], name: "index_sessions_on_cas_ticket"
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
+
+  create_table "stays", force: :cascade do |t|
+    t.integer  "person_id",                        null: false
+    t.integer  "housing_unit_id"
+    t.date     "arrived_at"
+    t.date     "departed_at"
+    t.boolean  "single_occupancy", default: false, null: false
+    t.boolean  "no_charge",        default: false, null: false
+    t.boolean  "waive_minimum",    default: false, null: false
+    t.integer  "percentage",       default: 100,   null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
+
+  add_index "stays", ["housing_unit_id"], name: "index_stays_on_housing_unit_id"
+  add_index "stays", ["person_id"], name: "index_stays_on_person_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",      default: "",        null: false
