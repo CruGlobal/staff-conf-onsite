dbd23c8ece6206e4b938b05fe2f31953c009f8be
Jon Sangster <jon@ertt.ca>
2016-12-13 22:49:07 -0400

    Fix self-referential foreign key

--- db/migrate/20161116222441_add_parent_id_to_ministries.rb
+++ db/migrate/20161116222441_add_parent_id_to_ministries.rb
@@ -1,5 +1,7 @@
 class AddParentIdToMinistries < ActiveRecord::Migration
   def change
-    add_reference :ministries, :parent, index: true, foreign_key: true
+    add_column :ministries, :parent_id, :integer, index: true
+    add_foreign_key :ministries, :ministries, column: :parent_id, primary_key: :id
+    add_index :ministries, :parent_id
   end
 end
