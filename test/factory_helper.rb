a8fdd254a5366939d885c0c6fb7ab7f2475073d3
Mitch Langlois <mitch@ballistiq.com>
2017-03-08 13:31:42 -0500

    Fix `random_future_date` to work when `nil` is passed in

--- test/factory_helper.rb
+++ test/factory_helper.rb
@@ -3,7 +3,8 @@ module FactoryHelper
     yield if Faker::Boolean.boolean
   end
 
-  def random_future_date(start_date=1.year.from_now)
+  def random_future_date(start_date=nil)
+    start_date ||= 1.year.from_now
     Faker::Date.between(start_date, start_date + 2.years)
   end
 end
