6dd08215d6fe75a8d8f5298259b350710d9e0fad
Mitch Langlois <mitch@ballistiq.com>
2017-03-08 13:13:06 -0500

    Creates FactoryHelper that is usable in all Factories

---
+++ test/factory_helper.rb
@@ -0,0 +1,9 @@
+module FactoryHelper
+  def maybe
+    yield if Faker::Boolean.boolean
+  end
+
+  def random_future_date(start_date=1.year.from_now)
+    Faker::Date.between(start_date, start_date + 2.years)
+  end
+end
