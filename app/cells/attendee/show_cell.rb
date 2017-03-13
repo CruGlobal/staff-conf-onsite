d083956140d2d87634325f825a0d591d042db736
Jon Sangster <jon@ertt.ca>
2017-03-13 14:58:16 -0300

    Create service to calculate a single Attendee Stay cost

--- app/cells/attendee/show_cell.rb
+++ app/cells/attendee/show_cell.rb
@@ -100,10 +100,33 @@ class Attendee::ShowCell < ::ShowCell
     panel 'Housing Costs (Temporary panel for demo)', class: 'TODO_panel' do
       result = ChargeAttendeeStays.call(attendee: attendee)
       if result.success?
+        temporary_stay_individual_dorms_cost_list
         temporary_stay_cost_table(result)
       else
         div(class: 'flash flash_error') { result.error }
       end
+    end
+  end
+
+  def temporary_stay_individual_dorms_cost_list
+    stays = attendee.stays
+    return if stays.empty?
+
+    h4 'Individual Stays:'
+    dl do
+      stays.each do |stay|
+        dt { join_stay_dates(stay) }
+        dd { temporary_stay_individual_dorms_cost_list_item(stay) }
+      end
+    end
+  end
+
+  def temporary_stay_individual_dorms_cost_list_item(stay)
+    result = SingleAttendeeStayCost.call(stay: stay)
+    if result.success?
+      text_node humanized_money_with_symbol result.total
+    else
+      div(class: 'flash flash_error') { result.error }
     end
   end
 
--- app/interactors/sum_attendee_stay_cost.rb
+++ app/interactors/sum_attendee_stay_cost.rb
@@ -8,41 +8,6 @@ class SumAttendeeStayCost
     context.charges ||= Hash.new { |h, v| h[v] = Money.empty }
   end
 
-  # If the housing facility for this individual assignment is an apartment,
-  # look for the "DO NO CHARGE FOR THIS ASSIGNMENT" flag - if YES, the charge =
-  # $0, and quit.
-  #
-  # For ALL dorm housing assignments (in case there's more than one), add up
-  # the TOTAL number of days living in a DORM (only). Call this the TOTAL Dorm
-  # Days.
-  #
-  # For each individual adult housing assignment:
-  #   1) Calculate length of stay in # days for this individual assignment:
-  #      arrival date minus departure date (result must be greater than 0)
-  #
-  #   2) Using Housing Facility from the assignment, determine the Cost Code.
-  #
-  #   3) If the housing facility is an APARTMENT, check to see if "WAIVE
-  #      MINIMUM STAY" is checked. If so, go to step 4. If MINIMUM STAY for this
-  #      housing facility is > length of stay, length of stay = MINIMUM STAY
-  #
-  #   4) Using the Cost Code, look up Adult $?/Day daily cost. A cost code may
-  #      have multiple cost "groups". The correct cost group will be determined
-  #      by choosing the group where the smallest Maximum Stay is >= either
-  #      (the apartment individual length of stay) or (the TOTAL Dorm Days).
-  #
-  #   5) If the housing facility for this individual assignment is a DORM, if
-  #      the "Single Occupancy" is YES, look up the "SINGLE UPCHARGE $/DAY" for
-  #      this length of stay (for an apartment type, the "SINGLE UPCHARGE
-  #      $/DAY" will always be $0).
-  #
-  #   6) Total cost is: individual length of stay X (daily cost + daily
-  #      upcharge)
-  #
-  #   7) If the housing facility for this individual assignment is an
-  #      apartment, multiply the total cost by the %COST THE OCCUPANT MUST PAY
-
-  # The final output is the resulting dollar amount.
   def call
     context.attendee.stays.each(&method(:sum_stay_cost))
 
@@ -52,53 +17,13 @@ class SumAttendeeStayCost
   private
 
   def sum_stay_cost(stay)
-    return if stay.housing_type == 'self_provided'
+    result = SingleAttendeeStayCost.call(stay: stay)
 
-    daily_cost = sum_daily_cost(stay)
-    if (type = charge_type(stay))
-      context.charges[type] += must_pay(stay, daily_cost * stay.duration)
+    if result.success?
+      context.charges[result.type] += result.total
     else
-      fail_no_charge_type!(stay)
+      context.fail!(error: context.error)
+      Money.empty
     end
-  end
-
-  def sum_daily_cost(stay)
-    cost_code = stay.housing_facility.try(:cost_code)
-
-    if (charge = cost_code.try(:charge, days: stay.duration))
-      if stay.housing_type == 'dormitory' && stay.single_occupancy?
-        charge.adult + charge.single_delta
-      else
-        charge.adult
-      end
-    else
-      fail_no_cost_code!(stay)
-    end
-  end
-
-  def fail_no_cost_code!(stay)
-    context.fail!(
-      error: format('%p does not have an associated cost code which can be ' \
-                    'applied to a stay of %d days',
-                    (stay.housing_facility || stay), stay.duration)
-    )
-  end
-
-  def charge_type(stay)
-    case stay.housing_type
-    when 'apartment' then 'apartment_rent'
-    when 'dormitory' then 'dorm_adult'
-    end
-  end
-
-  def fail_no_charge_type!(stay)
-    context.fail!(
-      error: format('%p has an unknown facility type: "%s"',
-                    (stay.housing_facility || stay), stay.housing_type)
-    )
-  end
-
-  def must_pay(stay, cost)
-    cost * (stay.housing_type == 'apartment' ? stay.must_pay_ratio : 1)
   end
 end
