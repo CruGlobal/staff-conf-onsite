bcd1f46e387a799840271dc8913bf8d159fc9d35
Jon Sangster <jon@ertt.ca>
2017-04-11 19:50:54 -0300

    Fix foreign_key migrations

--- db/migrate/20160812214527_create_meal_exemptions.rb
+++ db/migrate/20160812214527_create_meal_exemptions.rb
@@ -2,13 +2,12 @@ class CreateMealExemptions < ActiveRecord::Migration
   def change
     create_table :meal_exemptions do |t|
       t.date :date
-      t.references :attendee, references: :people, index: true
+      t.references :person, foreign_key: true, index: true
       t.string :meal_type
 
       t.timestamps
 
-      t.index [:attendee_id, :date, :meal_type], unique: true
-      t.foreign_key :people, column: :attendee_id
+      t.index [:person_id, :date, :meal_type], unique: true
     end
   end
 end
