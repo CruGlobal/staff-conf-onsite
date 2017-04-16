29ab10785cb06ca6d36381cac57e0efcd5f13b87
Jon Sangster <jon@ertt.ca>
2017-04-16 20:06:27 -0300

    Show valid keys on error

--- app/models/user_variable.rb
+++ app/models/user_variable.rb
@@ -14,7 +14,11 @@ class UserVariable < ActiveRecord::Base
 
     def get(short_name)
       cached_values[short_name.to_sym].tap do |val|
-        raise ArgumentError, "Unknown UserVariable, '#{short_name}'" if val.nil?
+        if val.nil?
+          raise ArgumentError,
+                format('Unknown UserVariable, %s (expected: %p)', short_name,
+                       cached_values.keys)
+        end
       end
     end
     alias [] get
