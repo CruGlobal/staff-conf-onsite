cd75fc04057ba5c8c54fc28ab3906091e0a594bb
Jon Sangster <jon@ertt.ca>
2017-04-24 02:54:00 -0300

    Show list of existing people on import

--- app/interactors/import/create_new_people_records.rb
+++ app/interactors/import/create_new_people_records.rb
@@ -4,6 +4,8 @@ module Import
   # [+context.import_people+ [+Array<Import::Person>+]]
   class CreateNewPeopleRecords
     include Interactor
+    include ActionView::Helpers::OutputSafetyHelper
+    include Rails.application.routes.url_helpers
 
     # Associates the row number a given record was created on, so in the event
     # of an error we can report to the end user which row of the spreadsheet
@@ -11,6 +13,7 @@ module Import
     RowRecord = Struct.new(:record, :row)
 
     Error = Class.new(StandardError)
+    PersonExistsError = Class.new(Error)
 
     before do
       @families = {}
@@ -25,35 +28,51 @@ module Import
         context.people = people.map(&:record)
         context.families = @families.values.map(&:record)
       end
+    rescue PersonExistsError
+      context.fail!(message: existing_people_message)
     rescue Error => e
       context.fail!(message: e.message)
     end
 
     private
 
+    def existing_people_message
+      safe_join [
+        'The imported file contains people that already exist in the database:',
+        existing_people_list
+      ]
+    end
+
+    def existing_people
+      @imports.each_with_index.map do |import, index|
+        find_existing_person(import, index)
+      end.compact
+    end
+
     def create_from_import(import, index)
       row = index + 2
-      find_or_create_person(import, row).tap do |p|
-        set_attributes(p.record, import)
-      end
+      create_person(import, row).tap { |p| set_attributes(p.record, import) }
+    rescue PersonExistsError
+      raise
     rescue StandardError => e
       raise Error, format('Row #%d: %s', row, e)
     end
 
-    def find_or_create_person(import, row)
+    def create_person(import, row)
       family = find_or_create_family(import, row)
 
-      existing_person = family.record.people.find do |p|
+      raise PersonExistsError if find_existing_person(import, row).present?
+
+      person = import.record_class.new.tap { |p| p.family = family.record }
+      RowRecord.new(person, row)
+    end
+
+    def find_existing_person(import, row)
+      family = find_or_create_family(import, row)
+
+      family.record.people.find do |p|
         p.birthdate == import.birthdate && p.first_name == import.first_name
       end
-
-      person =
-        if existing_person.present?
-          existing_person
-        else
-          import.record_class.new.tap { |p| p.family = family.record }
-        end
-      RowRecord.new(person, row)
     end
 
     def find_or_create_family(import, row)
@@ -109,6 +128,25 @@ module Import
       raise Error, format('Row #%d: Could not persist %s, %p. %s',
                           row_record.row, record.class.name, record.audit_name,
                           e.message)
+    end
+
+    def existing_people_list
+      Arbre::Context.new(receiver: self) do
+        ul do
+          receiver.send(:existing_people).each do |person|
+            li do
+              a(person.full_name, href: receiver.send(:person_path, person))
+            end
+          end
+        end
+      end
+    end
+
+    def person_path(person)
+      case person
+      when Attendee then attendee_path(person)
+      when Child then child_path(person)
+      end
     end
   end
 end
