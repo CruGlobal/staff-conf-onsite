2955601805cff8b400c061b8ad3e393bbcf81357
Jon Sangster <jon@ertt.ca>
2017-05-04 12:46:31 -0300

    General users can edit HousingUnits, but finance cannot

--- app/policies/housing_unit_policy.rb
+++ app/policies/housing_unit_policy.rb
@@ -1,3 +1,14 @@
 # The security policy for accessing {HousingUnit} records.
-class HousingUnitPolicy < AdminOnlyPolicy
+class HousingUnitPolicy < GeneralPolicy
+  def create?
+    !user.finance?
+  end
+
+  def update?
+    !user.finance?
+  end
+
+  def destroy?
+    !user.finance?
+  end
 end
