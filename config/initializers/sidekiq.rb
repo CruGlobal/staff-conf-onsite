6e5ab9401c440792433fa8c54027ef2e096396da
Josh Starcher <josh.starcher@gmail.com>
2017-05-15 12:21:19 -0400

    User redis for session storage.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,15 @@
 require Rails.root.join('config', 'initializers', 'redis').to_s
 
+redis_config = { url: Redis.current.client.id,
+                   namespace: "sco:#{Rails.env}:sidekiq" }
+
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: Redis.current.client.id,
-                   namespace: "MPDX:#{Rails.env}:resque" }
+  config.redis = redis_config
 end
 
 Sidekiq.configure_server do |config|
   Sidekiq::Logging.logger.level = Logger::FATAL unless Rails.env.development?
   Rails.logger = Sidekiq::Logging.logger
-  config.redis = { url: Redis.current.client.id,
-                   namespace: "Staff-Conf:#{Rails.env}:resque" }
+  config.redis = redis_config
 end
