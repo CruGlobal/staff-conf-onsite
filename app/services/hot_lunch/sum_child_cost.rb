821c86c3b8d486ebb70d2b7bcfa35611359aade8
Jon Sangster <jon@ertt.ca>
2017-05-17 22:44:47 -0300

    Add hot lunch week variables

--- app/services/hot_lunch/sum_child_cost.rb
+++ app/services/hot_lunch/sum_child_cost.rb
@@ -1,43 +1,39 @@
 class HotLunch::SumChildCost < ChargesService
-  AGE_RANGE = 3..4
+  REQUIRED_AGES = 3..4
 
   attr_accessor :child
 
   def call
-    charges[:lunch] +=
-      if applicable?
-        hot_lunch_costs.inject(Money.empty, :+)
-      else
-        Money.empty
-      end
-
+    charges[:lunch] += hot_lunch_costs.inject(Money.empty, :+)
     self.cost_adjustments = child.cost_adjustments
   end
 
   private
 
-  def applicable?
-    AGE_RANGE.include?(child.age)
-  end
-
   def hot_lunch_costs
-    hot_lunch_in_dorm_indexes.map do |index|
+    applicable_hot_lunch_indexes.map do |index|
       UserVariable["hot_lunch_week_#{index}"]
     end
   end
 
-  # @see Childcare#CHILDCARE_WEEKS
-  def hot_lunch_in_dorm_indexes
+  # Charges are applicable unless the child was in a dorm at the start of the
+  # week. One exception: for children of {#REQUIRED_AGES}, charges are always
+  # applicable
+  def applicable_hot_lunch_indexes
     hot_lunch_start_dates.
-      map { |index, date| index unless in_dorm_at?(date) }.
+      map { |index, date| index unless !required? && in_dorm_at?(date) }.
       compact
+  end
+
+  def required?
+    REQUIRED_AGES.include?(child.age)
   end
 
   # @return [Hash<Integer,Date>]
   def hot_lunch_start_dates
     Hash[
       child.hot_lunch_weeks.map do |week_offset|
-        [week_offset, UserVariable[:childcare_first_day] + week_offset.weeks]
+        [week_offset, UserVariable["hot_lunch_begin_#{week_offset}"]]
       end
     ]
   end
