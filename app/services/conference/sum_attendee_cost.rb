87ff499cc7bd77c5b32e08158cb9f6bce3049903
Jon Sangster <jon@ertt.ca>
2017-05-18 17:36:19 -0300

    Only Attendees have conferences

--- app/services/conference/sum_attendee_cost.rb
+++ app/services/conference/sum_attendee_cost.rb
@@ -1,16 +1,16 @@
 # == Context Input
 #
 # [+context.attendee+ [+Attendee+]]
-class Conference::SumPersonCost < ChargesService
-  attr_accessor :person
+class Conference::SumAttendeeCost < ChargesService
+  attr_accessor :attendee
 
   def call
     charges[:tuition_track] +=
       # TODO: exclude staff conference once it's added
-      person.conferences.
+      attendee.conferences.
         map(&:price).
         inject(Money.empty, &:+)
 
-    self.cost_adjustments = person.cost_adjustments
+    self.cost_adjustments = attendee.cost_adjustments
   end
 end
