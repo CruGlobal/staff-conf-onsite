bb495eddccf0173805c7a03252b18dd5d300e08a
Josh Starcher <josh.starcher@gmail.com>
2017-05-25 17:25:59 -0400

    Add reports screen/download.

--- db/migrate/20170525140950_create_reports.rb
+++ db/migrate/20170525140950_create_reports.rb
@@ -1,9 +1,10 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-      t.string :name
-      t.text :query
-      t.string :role
+      t.string :category, null: false
+      t.string :name, null: false
+      t.text :query, null: false
+      t.string :role, null: false, default: 'general'
 
       t.timestamps null: false
     end
