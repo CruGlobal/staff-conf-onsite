fdc60c5e70522eaeac904645d5695d84ed350920
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2018-12-20 17:42:11 -0800

    Add model with basic validations

---
+++ app/models/childcare_envelope.rb
@@ -0,0 +1,13 @@
+class ChildcareEnvelope < ActiveRecord::Base
+  belongs_to :child
+  belongs_to :recipient, class_name: 'Attendee'
+
+  validates :envelope_id, :status, presence: true
+  validate :must_belong_to_same_family
+
+  def must_belong_to_same_family
+    if child.family_id != recipient.family_id
+      errors.add(:child, "is not part of the recipients family")
+    end
+  end
+end
---
+++ test/models/childcare_envelope_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class ChildcareEnvelopeTest < ModelTestCase
+
+  def setup
+    @attendee = build :attendee, family_id: 1
+    @child = build :child, family_id: 1
+    @subject = ChildcareEnvelope.new(envelope_id: "1234", status: "sent", recipient: @attendee, child: @child)
+  end
+
+  test 'valid childcare envelope' do
+    assert @subject.valid?
+  end
+
+  test 'invalid without a docusign envelope id' do
+    @subject.envelope_id = nil
+
+    refute @subject.valid?
+    assert_not_nil @subject.errors[:envelope_id], "can't be blank"
+  end
+
+  test 'invalid without a status' do
+    @subject.status = nil
+
+    refute @subject.valid?
+    assert_not_nil @subject.errors[:status], "can't be blank"
+  end
+
+  test 'invalid if recipient and child do not belong to the same family' do
+    @child.family_id = 2
+
+    refute @subject.valid?
+    assert_not_nil @subject.errors[:child], 'is not part of the recipients family'
+  end
+end
