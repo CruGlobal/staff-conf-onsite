4be70327479504ae89bcc5c20929fba46a0aa655
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-01-02 15:22:58 -0800

    PR Review notes 1:
    * Pass exception directly to Rollbar so it can extract all the data it
    can use

--- app/jobs/docusign/send_childcare_envelope_job.rb
+++ app/jobs/docusign/send_childcare_envelope_job.rb
@@ -3,14 +3,14 @@ class Docusign::SendChildcareEnvelopeJob < ApplicationJob
 
   # Stop from retrying if valid envelope already exists
   rescue_from(Childcare::SendDocusignEnvelope::SendEnvelopeError) do |exception|
-    Rollbar.error "[#{self.class.name}] Something went wrong with you job: #{exception}"
-    Rails.logger.error "[#{self.class.name}] Something went wrong with you job: #{exception}"
+    Rollbar.error(exception)
+    Rails.logger.error "[#{self.class.name}] Something went wrong with your job: #{exception}"
   end
 
   # Stop from retrying if error on docusign configuration
   rescue_from(Docusign::CreateEnvelopeFromTemplate::DocusignError) do |exception|
-    Rollbar.error "[#{self.class.name}] Something went wrong with you job: #{exception}"
-    Rails.logger.error "[#{self.class.name}] Something went wrong with you job: #{exception}"
+    Rollbar.error(exception)
+    Rails.logger.error "[#{self.class.name}] Something went wrong with your job: #{exception}"
   end
 
   def perform(child)
