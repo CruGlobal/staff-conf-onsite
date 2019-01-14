07fb821099db6d008b0249eebeffbc4f6098bd41
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-01-14 12:35:41 -0800

    Add basic confirm charges email

--- app/mailers/precheck_mailer.rb
+++ app/mailers/precheck_mailer.rb
@@ -11,9 +11,28 @@ class PrecheckMailer < ApplicationMailer
     mail(to: email, subject: 'Cru19 - Precheck Completed')
   end
 
+  def confirm_charges(family)
+    @token = create_or_refresh_token(family)
+    email = if Rails.env.production?
+              family.attendees.pluck(:email).select(&:present?).compact
+            else
+              DEFAULT_EMAIL
+            end
+    mail(to: email, subject: 'Cru19 Precheck Confirmation Email')
+  end
+
   def changes_requested(family, message)
     @family = family
     @message = message
     mail(to: DEFAULT_CHANGES_REQUEST_EMAIL, subject: "Cru19 Precheck Modification Request")
+  end
+
+  private
+
+  def create_or_refresh_token(family)
+    if family.precheck_email_token.present?
+      family.precheck_email_token.delete
+    end
+    family.create_precheck_email_token
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,4 +45,5 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
