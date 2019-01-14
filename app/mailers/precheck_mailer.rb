a1aacce3db2cb7cbdeced07ba22dc20bcaf4e7cf
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-01-14 12:33:15 -0800

    Add mailer to notify staff of changes requested by family

--- app/mailers/precheck_mailer.rb
+++ app/mailers/precheck_mailer.rb
@@ -1,5 +1,6 @@
 class PrecheckMailer < ApplicationMailer
   DEFAULT_EMAIL = 'josh.starcher@cru.org'.freeze
+  DEFAULT_CHANGES_REQUEST_EMAIL = 'josh.starcher@cru.org'.freeze
 
   def precheck_completed(family)
     email = if Rails.env.production?
@@ -8,5 +9,11 @@ class PrecheckMailer < ApplicationMailer
               DEFAULT_EMAIL
             end
     mail(to: email, subject: 'Cru19 - Precheck Completed')
+  end
+
+  def changes_requested(family, message)
+    @family = family
+    @message = message
+    mail(to: DEFAULT_CHANGES_REQUEST_EMAIL, subject: "Cru19 Precheck Modification Request")
   end
 end
