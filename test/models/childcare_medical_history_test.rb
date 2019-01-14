cae7796efd5aa24a4c6c0050a6e07b2578646b2d
Kurtis Harper <kurtis@ballistiq.com>
2019-01-14 16:12:14 -0400

    Added a couple of model tests for Child and student medical history

--- test/models/childcare_medical_history_test.rb
+++ test/models/childcare_medical_history_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ChildcareMedicalHistoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @person = Person.new(first_name: 'test-name')
+    @childcare = ChildcareMedicalHistory.create(allergy: 'allergy-text')
+  end
+
+  test 'childcare medical' do
+    assert_equal 'test-name', @person.first_name
+    @person.childcare_medical_history = @childcare
+    assert_equal 'allergy-text', @person.childcare_medical_history.allergy
+  end
 end
--- test/models/cru_student_medical_history_test.rb
+++ test/models/cru_student_medical_history_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CruStudentMedicalHistoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @person = Person.new(first_name: 'test-name')
+    @cru_student = CruStudentMedicalHistory.create(parent_agree: 'Yes')
+  end
+
+  test 'cru student agree' do
+    assert_equal 'test-name', @person.first_name
+    @person.cru_student_medical_history = @cru_student
+    assert_equal 'Yes', @person.cru_student_medical_history.parent_agree
+  end
 end
