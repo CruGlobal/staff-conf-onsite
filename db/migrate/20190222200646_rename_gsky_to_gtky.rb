d514c37dbe3e597adb0a7c1851825d26a0c016bc
Sheldon D <sheldond@users.noreply.github.com>
2019-02-22 15:35:34 -0500

    Fix naming of GTKY

---
+++ db/migrate/20190222200646_rename_gsky_to_gtky.rb
@@ -0,0 +1,40 @@
+# GTKY is an acronym that stands for Getting To Know You.
+class RenameGskyToGtky < ActiveRecord::Migration
+  COLUMNS_NAMES = %w[
+    gsky_lunch
+    gsky_signout
+    gsky_sibling_signout
+    gsky_sibling
+    gsky_small_group_friend
+    gsky_musical
+    gsky_activities
+    gsky_gain
+    gsky_growth
+    gsky_addl_info
+    gsky_challenges
+    gsky_large_groups
+    gsky_small_groups
+    gsky_leader
+    gsky_follower
+    gsky_friends
+    gsky_hesitant
+    gsky_active
+    gsky_reserved
+    gsky_boundaries
+    gsky_authority
+    gsky_adapts
+    gsky_allergies
+  ]
+
+  def up
+    COLUMNS_NAMES.each do |columns_name|
+      rename_column :cru_student_medical_histories, columns_name, columns_name.gsub('gsky', 'gtky')
+    end
+  end
+
+  def down
+    COLUMNS_NAMES.each do |columns_name|
+      rename_column :cru_student_medical_histories, columns_name.gsub('gsky', 'gtky'), columns_name
+    end
+  end
+end
