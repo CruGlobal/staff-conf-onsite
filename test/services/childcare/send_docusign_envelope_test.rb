f28360c8fcde4438cfe3c9962a7c87de0678477f
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-02 21:09:09 -0700

    Add tests for logic in send docusign envelope

--- test/services/childcare/send_docusign_envelope_test.rb
+++ test/services/childcare/send_docusign_envelope_test.rb
@@ -30,4 +30,64 @@ class Childcare::SendDocusignEnvelopeTest < ServiceTestCase
       assert_equal(before, ChildcareEnvelope.count)
     end
   end
+
+  test '#determine_docusign_template for a child on childcare grade and no misc health issues checked' do
+    @child.grade_level = 'grade5'
+    ChildcareMedicalHistory.new(child: @child, health_misc: ['None of the above'])
+    docusign_envelope = Childcare::SendDocusignEnvelope.new(@child)
+
+    stub_envelope_const('CARECAMP_TEMPLATE', 'template_key_stub') do
+      assert_equal 'template_key_stub', docusign_envelope.send(:determine_docusign_template)
+    end
+  end
+
+  test '#determine_docusign_template for a child on childcare grade and a misc health issue checked' do
+    @child.grade_level = 'grade5'
+    ChildcareMedicalHistory.new(child: @child, health_misc: ['Disability'])
+    docusign_envelope = Childcare::SendDocusignEnvelope.new(@child)
+
+    stub_envelope_const('CARECAMP_VIP_TEMPLATE', 'template_key_stub') do
+      assert_equal 'template_key_stub', docusign_envelope.send(:determine_docusign_template)
+    end
+  end
+
+  test '#determine_docusign_template for a child on senior grade and only no misc health issues checked on health misc' do
+    @child.grade_level = 'grade6'
+    CruStudentMedicalHistory.new(child: @child, cs_health_misc: ['None of the above'])
+    docusign_envelope = Childcare::SendDocusignEnvelope.new(@child)
+
+    stub_envelope_const('CRUSTU_TEMPLATE', 'template_key_stub') do
+      assert_equal 'template_key_stub', docusign_envelope.send(:determine_docusign_template)
+    end
+  end
+
+  test '#determine_docusign_template for a child on senior grade and a misc health issue checked' do
+    @child.grade_level = 'grade6'
+    CruStudentMedicalHistory.new(child: @child, cs_health_misc: ['Developmental delay', 'Sensory issues'])
+    docusign_envelope = Childcare::SendDocusignEnvelope.new(@child)
+
+    stub_envelope_const('CRUSTU_VIP_TEMPLATE', 'template_key_stub') do
+      assert_equal 'template_key_stub', docusign_envelope.send(:determine_docusign_template)
+    end
+  end
+
+  test '#determine_docusign_template when no answer for misc health issue, defaults to VIP template' do
+    @child.grade_level = 'grade2'
+    ChildcareMedicalHistory.new(child: @child, health_misc: nil)
+    docusign_envelope = Childcare::SendDocusignEnvelope.new(@child)
+
+    stub_envelope_const('CARECAMP_VIP_TEMPLATE', 'template_key_stub') do
+      assert_equal 'template_key_stub', docusign_envelope.send(:determine_docusign_template)
+    end
+  end
+
+  def stub_envelope_const(const, value)
+    old = Childcare::SendDocusignEnvelope.const_get(const)
+    Childcare::SendDocusignEnvelope.send(:remove_const, const)
+    Childcare::SendDocusignEnvelope.const_set(const, value)
+    yield
+  ensure
+    Childcare::SendDocusignEnvelope.send(:remove_const, const)
+    Childcare::SendDocusignEnvelope.const_set(const, old)
+  end
 end
