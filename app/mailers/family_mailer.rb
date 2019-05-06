8c6b420737109a9fb2c8a69bdcb284faf7f2ea47
Sheldon D <sheldond@users.noreply.github.com>
2019-05-06 14:59:48 -0400

    Replace default email with interceptor

--- app/mailers/family_mailer.rb
+++ app/mailers/family_mailer.rb
@@ -1,6 +1,4 @@
 class FamilyMailer < ApplicationMailer
-  DEFAULT_EMAIL = 'josh.starcher@cru.org'.freeze
-
   add_template_helper(ApplicationHelper)
   add_template_helper(CostAdjustmentHelper)
   add_template_helper(PaymentHelper)
@@ -13,24 +11,16 @@ class FamilyMailer < ApplicationMailer
     finance_user = User.find_by(role: 'finance')
     @policy = Pundit.policy(finance_user, family)
 
-    emails =
-      if Rails.env.production?
-        family.attendees.pluck(:email).select(&:present?).compact
-      else
-        DEFAULT_EMAIL
-      end
+    emails = family.attendees.pluck(:email).select(&:present?).compact
+
     mail(to: emails, subject: 'Cru17 Financial Summary')
   end
 
   def media_release(family)
     @family = family
 
-    emails =
-      if Rails.env.production?
-        family.attendees.pluck(:email).select(&:present?).compact
-      else
-        DEFAULT_EMAIL
-      end
+    emails = family.attendees.pluck(:email).select(&:present?).compact
+
     mail(to: emails,
          from: 'cru17.mediaReleases@cru.org',
          subject: 'IMPORTANT: Please Read')
