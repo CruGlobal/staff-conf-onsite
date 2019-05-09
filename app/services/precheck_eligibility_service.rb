6673fee984cfdbd569805a17921cb2a66a4b14cf
Sheldon D <sheldond@users.noreply.github.com>
2019-05-08 20:58:50 -0400

    If a family is not eligible for precheck they should be able to submit an email to helpdesk through the precheck page

--- app/services/precheck_eligibility_service.rb
+++ app/services/precheck_eligibility_service.rb
@@ -2,8 +2,7 @@ class PrecheckEligibilityService < ApplicationService
   attr_accessor :family
 
   def call
-    pending_approval? &&
-      within_time_window? &&
+    within_time_window? &&
       children_approved? &&
       chargeable_staff_number? &&
       housing_preference_confirmed?
@@ -11,7 +10,6 @@ class PrecheckEligibilityService < ApplicationService
 
   private
 
-  def_delegator :family, :pending_approval?
   def_delegator :family, :attendees
   def_delegator :family, :children
   def_delegator :family, :chargeable_staff_number?
