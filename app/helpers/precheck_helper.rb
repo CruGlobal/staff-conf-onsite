080e5d8b0ee583f438d44a8cab03d83e5bc74ab7
Sheldon D <sheldond@users.noreply.github.com>
2019-05-09 11:03:50 -0400

    Improve display of precheck details in activeadmin

--- app/helpers/precheck_helper.rb
+++ app/helpers/precheck_helper.rb
@@ -2,4 +2,15 @@ module PrecheckHelper
   def precheck_eligible?(family)
     PrecheckEligibilityService.new(family: family).call
   end
+
+  def precheck_status_label(arg)
+    status = arg.is_a?(Family) ? arg.precheck_status : arg
+    status.titleize
+  end
+
+  def precheck_statuses_select_collection
+    Family.precheck_statuses.map do |name, _id|
+      [precheck_status_label(name), name]
+    end
+  end
 end
