cdfdee8c3f225d03caeaa23b5b9a82204c771553
Sheldon D <sheldond@users.noreply.github.com>
2019-05-09 12:44:21 -0400

    Refactor precheck controller into multiple controllers

---
+++ test/controllers/precheck/rejection_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+require_relative '../../../db/user_variables'
+
+class Precheck::RejectionControllerTest < ControllerTestCase
+  include ActionMailer::TestHelper
+
+  stub_user_variable conference_id: 'MyConfName',
+                     support_email: 'support@example.org',
+                     conference_logo_url: 'https://www.logo.com',
+                     mail_interceptor_email_addresses: ['test@test.com']
+
+  test '#create with an invalid auth token get error page' do
+    post :create, token: "sdfsdf"
+    assert_response :success
+    assert_template :error
+  end
+
+  test '#create with a valid token sends change request email' do
+    token = create(:precheck_email_token)
+
+    assert_equal token.family.reload.precheck_status, 'pending_approval'
+    assert_no_difference('PrecheckEmailToken.count') do
+      assert_emails 1 do
+        post :create, token: token.token, message: "My name is misspelled"
+      end
+    end
+    assert_equal token.family.reload.precheck_status, 'changes_requested'
+
+    assigns(:family)
+    last_email = ActionMailer::Base.deliveries.last
+    assert_equal "MyConfName - Precheck Modification Request", last_email.subject
+  end
+end
