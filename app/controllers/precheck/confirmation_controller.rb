587d9c148735ec253660eda467d9c70adfdc8a43
Sheldon D <sheldond@users.noreply.github.com>
2019-05-10 18:00:09 -0400

    Precheck updates round two (#138)
    * Send family email if $ or Forms are not precheck ready
    
    * Update precheck mailer views
    
    * Precheck errors not actionable if checked-in or outside window
    
    * Family is not precheck eligible if changes requested or accepted
    
    * Removing hardcoded year

--- app/controllers/precheck/confirmation_controller.rb
+++ app/controllers/precheck/confirmation_controller.rb
@@ -1,6 +1,5 @@
 class Precheck::ConfirmationController < PrecheckController
   def create
     @family.update!(precheck_status: :approved)
-    PrecheckMailer.precheck_completed(@family).deliver_now
   end
 end
