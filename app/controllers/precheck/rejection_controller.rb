05f6cf2ca844eec7f484c3426ccb7d21212205d0
Sheldon D <sheldond@users.noreply.github.com>
2019-05-13 16:56:05 -0400

    When family confirms precheck attendees should be checked-in

--- app/controllers/precheck/rejection_controller.rb
+++ app/controllers/precheck/rejection_controller.rb
@@ -1,6 +1,7 @@
 class Precheck::RejectionController < PrecheckController
   def create
     @family.update!(precheck_status: :changes_requested)
+    UpdatedFamilyPrecheckStatusService.new(family: @family).call
     PrecheckMailer.changes_requested(@family, params['message']).deliver_now
   end
 end
