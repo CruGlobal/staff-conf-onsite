87b4c5844652e19c12ca0d465a54d72f8927f20a
Sheldon D <sheldond@users.noreply.github.com>
2019-05-14 15:35:40 -0400

    Improve mailer from and to

--- app/mailers/precheck_mailer.rb
+++ app/mailers/precheck_mailer.rb
@@ -7,22 +7,21 @@ class PrecheckMailer < ApplicationMailer
     @finances = FamilyFinances::Report.call(family: family)
     @policy = Pundit.policy(User.new(role: 'finance'), family)
 
-    to_email = family.attendees.pluck(:email).select(&:present?).compact
-    mail(to: to_email, subject: t('.subject', conference: UserVariable[:conference_id]))
+    mail(to: to_family_attendees(family), subject: t('.subject', conference: UserVariable[:conference_id]))
   end
 
   def changes_requested(family, message)
     @family = family
     @message = message
-    email = UserVariable[:support_email]
-    mail(to: email, subject: t('.subject', conference: UserVariable[:conference_id], family: @family.to_s))
+
+    mail(to: UserVariable[:support_email], subject: t('.subject', conference: UserVariable[:conference_id], family: @family.to_s))
   end
 
   def report_issues(family)
     @family = family
     @token = find_token(family)
-    to_email = family.attendees.pluck(:email).select(&:present?).compact
-    mail(to: to_email, subject: t('.subject', conference: UserVariable[:conference_id]))
+
+    mail(to: to_family_attendees(family), subject: t('.subject', conference: UserVariable[:conference_id]))
   end
 
   private
