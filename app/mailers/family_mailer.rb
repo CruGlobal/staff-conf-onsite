700847dddb31d596206ccc723e7be6e0218e5e87
Sheldon D <sheldond@users.noreply.github.com>
2019-05-14 15:35:40 -0400

    Improve mailer from and to

--- app/mailers/family_mailer.rb
+++ app/mailers/family_mailer.rb
@@ -8,15 +8,14 @@ class FamilyMailer < ApplicationMailer
     @finances = FamilyFinances::Report.call(family: family)
     @policy = Pundit.policy(User.new(role: 'finance'), family)
 
-    emails = family.attendees.pluck(:email).select(&:present?).compact
-    mail(to: emails, subject: t('.subject', conference: UserVariable[:conference_id]))
+    mail(to: to_family_attendees(family), subject: t('.subject', conference: UserVariable[:conference_id]))
   end
 
   # def media_release(family)
   #   @family = family
-
+  #
   #   emails = family.attendees.pluck(:email).select(&:present?).compact
-
+  #
   #   mail(to: emails,
   #        from: 'cru17.mediaReleases@cru.org',
   #        subject: 'IMPORTANT: Please Read')
--- test/mailers/family_mailer_test.rb
+++ test/mailers/family_mailer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
+require_relative '../../db/user_variables'
+
 class FamilyMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    SeedUserVariables.new.call
+    UserVariable.find_by(short_name: :mail_interceptor_email_addresses).update!(value: [])
+    @family = create(:family_with_members)
+  end
+
+  test '#summary' do
+    email = FamilyMailer.summary(@family).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
+
+    assert_equal ['no-reply@cru.org'], email.from
+    assert_equal @family.attendees.map(&:email).sort, email.to.sort
+    assert_equal 'Cru17 Financial Summary', email.subject
+    assert_match 'Remaining Balance Due', email.body.to_s
+  end
 end
