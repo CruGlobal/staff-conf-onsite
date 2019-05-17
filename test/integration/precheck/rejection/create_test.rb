00dfc8c2acbf2b126d14e3ede8576b41d56494f2
Sheldon D <sheldond@users.noreply.github.com>
2019-05-17 17:00:53 -0400

    Change the text of the "Confirm..." and "Request Changes" buttons

--- test/integration/precheck/rejection/create_test.rb
+++ test/integration/precheck/rejection/create_test.rb
@@ -17,7 +17,7 @@ class Precheck::RejectionController::CreateTest < IntegrationTest
     enable_javascript!
 
     visit precheck_status_path(token: @eligible_family.precheck_email_token.token)
-    click_link 'Request Changes...'
+    click_link 'No, I need to request changes...'
     fill_in 'message', with: 'Testing message.'
     click_button 'Submit Request'
     page.driver.browser.switch_to.alert.accept
