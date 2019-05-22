3d97246ba6dd18f56523ebcbaeb5d706b7ba00e9
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-22 09:24:52 -0700

    Check precheck status on reject button
    If the user rejects the precheck email, and the precheck conditions are
    not met anymore, we now redirect to the status page.
    This will make it clearer if user opens page and clicks the reject
    button days later when the conditions might not be met anymore.

--- test/controllers/precheck/rejection_controller_test.rb
+++ test/controllers/precheck/rejection_controller_test.rb
@@ -18,6 +18,7 @@ class Precheck::RejectionControllerTest < ControllerTestCase
 
   test '#create with a valid token sends change request email' do
     token = create(:precheck_email_token)
+    PrecheckEligibilityService.stubs(:new).returns(stub(call: true))
 
     assert_equal token.family.reload.precheck_status, 'pending_approval'
     assert_no_difference('PrecheckEmailToken.count') do
@@ -35,6 +36,7 @@ class Precheck::RejectionControllerTest < ControllerTestCase
   test '#create when already in changes_requested state sends another change request email' do
     token = create(:precheck_email_token)
     token.family.update!(precheck_status: :changes_requested)
+    PrecheckEligibilityService.stubs(:new).returns(stub(call: true))
 
     assert_equal token.family.reload.precheck_status, 'changes_requested'
     assert_emails 1 do
@@ -45,5 +47,19 @@ class Precheck::RejectionControllerTest < ControllerTestCase
     last_email = ActionMailer::Base.deliveries.last
     assert_equal "MyConfName PreCheck Changes Requested for Family #{token.family}", last_email.subject
     assert_equal [UserVariable[:support_email]], last_email.to
+  end
+
+  test '#create when precheck eligibility is false' do
+    token = create(:precheck_email_token)
+    @attendee = create(:attendee, family: token.family)
+    PrecheckEligibilityService.stubs(:new).returns(stub(call: false))
+
+    assert_equal @attendee.family.precheck_status, 'pending_approval'
+
+    assert_no_difference -> { ActionMailer::Base.deliveries.size } do
+      post :create, token: token.token
+    end
+    assert_equal @attendee.family.reload.precheck_status, 'pending_approval'
+    assert_redirected_to precheck_status_path
   end
 end
