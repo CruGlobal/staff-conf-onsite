3cb1e50e08da8899502bf4c10766f20732f65ca1
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-21 17:46:30 -0700

    Precheck confirm button checks eligibility every time
    If eligibility changes between the user opening the email and clicking
    on the action, the user is redirected to a status page notifying of
    the error.

--- app/controllers/precheck/confirmation_controller.rb
+++ app/controllers/precheck/confirmation_controller.rb
@@ -1,5 +1,7 @@
 class Precheck::ConfirmationController < PrecheckController
   def create
+    redirect_to precheck_status_path and return unless PrecheckEligibilityService.new(family: @family).call
+
     @family.update!(precheck_status: :approved)
     UpdatedFamilyPrecheckStatusService.new(family: @family).call
   end
