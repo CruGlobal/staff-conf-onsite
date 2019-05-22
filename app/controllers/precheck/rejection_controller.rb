5ffc2c8079b0e40fedd4500d785ee692e3a38925
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-22 12:21:33 -0700

    Rejection controller should only redirect to status path when too late

--- app/controllers/precheck/rejection_controller.rb
+++ app/controllers/precheck/rejection_controller.rb
@@ -1,6 +1,6 @@
 class Precheck::RejectionController < PrecheckController
   def create
-    redirect_to precheck_status_path and return unless PrecheckEligibilityService.new(family: @family).call
+    redirect_to precheck_status_path and return if PrecheckEligibilityService.new(family: @family).too_late?
 
     @family.update!(precheck_status: :changes_requested)
     @message = params['message']
