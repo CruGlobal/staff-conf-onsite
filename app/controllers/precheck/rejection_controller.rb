9774f39ecb9d9a8291a6edfe3a6a8928535b9f79
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-22 09:24:52 -0700

    Check precheck status on reject button
    If the user rejects the precheck email, and the precheck conditions are
    not met anymore, we now redirect to the status page.
    This will make it clearer if user opens page and clicks the reject
    button days later when the conditions might not be met anymore.

--- app/controllers/precheck/rejection_controller.rb
+++ app/controllers/precheck/rejection_controller.rb
@@ -1,5 +1,7 @@
 class Precheck::RejectionController < PrecheckController
   def create
+    redirect_to precheck_status_path and return unless PrecheckEligibilityService.new(family: @family).call
+
     @family.update!(precheck_status: :changes_requested)
     @message = params['message']
     UpdatedFamilyPrecheckStatusService.new(family: @family, message: @message).call
