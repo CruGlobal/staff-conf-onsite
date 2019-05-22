bf3d775d524c9f368b6a32ddd82f9875c3bee69a
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-22 14:12:49 -0700

    Bump schema and add irreversible migration error

--- db/migrate/20190522165029_fix_reports_table_pkey_sequence.rb
+++
@@ -1,8 +0,0 @@
-class FixReportsTablePkeySequence < ActiveRecord::Migration
-  def up
-    execute "SELECT SETVAL('ministries_id_seq', (SELECT MAX(id) FROM ministries));" 
-  end
-
-  def down
-  end
-end
