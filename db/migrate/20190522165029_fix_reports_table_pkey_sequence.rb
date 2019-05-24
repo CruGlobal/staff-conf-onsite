dab24873706da87765b0eb35de5ec5db716bf6b8
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-24 11:43:15 -0700

    Correction to table pkey sequence fix
    Target table is reports not ministries

--- db/migrate/20190522165029_fix_reports_table_pkey_sequence.rb
+++ db/migrate/20190522165029_fix_reports_table_pkey_sequence.rb
@@ -1,6 +1,6 @@
 class FixReportsTablePkeySequence < ActiveRecord::Migration
   def up
-    execute "SELECT SETVAL('ministries_id_seq', (SELECT MAX(id) FROM ministries));" 
+    execute "SELECT SETVAL('reports_id_seq', (SELECT MAX(id) FROM reports));" 
   end
 
   def down
