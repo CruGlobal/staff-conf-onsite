7287dcd78ef1063bad0a76f16185a1976a255b0d
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-28 10:17:44 -0700

    Refactor to move too_late eligibility check to service

--- app/controllers/precheck/rejection_controller.rb
+++ app/controllers/precheck/rejection_controller.rb
@@ -1,16 +1,9 @@
 class Precheck::RejectionController < PrecheckController
   def create
-    redirect_to precheck_status_path and return if inelegible_for_precheck?
+    redirect_to precheck_status_path and return if PrecheckEligibilityService.new(family: @family).too_late_or_checked_in?
 
     @family.update!(precheck_status: :changes_requested)
     @message = params['message']
     UpdatedFamilyPrecheckStatusService.new(family: @family, message: @message).call
-  end
-
-  private
-
-  def inelegible_for_precheck?
-    elegibility_checker = PrecheckEligibilityService.new(family: @family)
-    elegibility_checker.too_late? || elegibility_checker.checked_in_already?
   end
 end
