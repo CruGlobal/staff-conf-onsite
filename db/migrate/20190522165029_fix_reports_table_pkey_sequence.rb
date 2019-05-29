2d709cad439963342e290e248f63b2ce6797b88a
Sheldon D <sheldond@users.noreply.github.com>
2019-05-29 14:42:59 -0400

    Remove duplicate migration file

--- db/migrate/20190522165029_fix_reports_table_pkey_sequence.rb
+++
@@ -1,9 +0,0 @@
-class FixReportsTablePkeySequence < ActiveRecord::Migration
-  def up
-    execute "SELECT SETVAL('reports_id_seq', (SELECT MAX(id) FROM reports));" 
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
