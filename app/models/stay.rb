b1204f1aad5be2c482da04e32a571064457c4c04
Cristian Guerrero <cristian.guerrero@ballistiq.com>
2019-05-31 09:35:30 -0700

    Refactor stay method

--- app/models/stay.rb
+++ app/models/stay.rb
@@ -111,7 +111,8 @@ class Stay < ApplicationRecord
   end
 
   def to_s(without_unit: false)
-    where = without_unit ? (housing_facility&.to_s || 'Self-Provided') : (housing_unit&.to_s || 'Self-Provided')
+    where = without_unit ? housing_facility : housing_unit
+    where = where || 'Self-Provided'
 
     format('%s, %s – %s', where, arrived_at, departed_at)
   end
