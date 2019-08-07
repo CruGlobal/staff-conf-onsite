a4eadfced66575790343f345edf15f44ef78d99e
Josh Starcher <josh.starcher@gmail.com>
2019-08-07 16:01:15 -0400

    Fix bug in Account Report.

--- app/services/accounting_report/child_stay_taxable_costs.rb
+++ app/services/accounting_report/child_stay_taxable_costs.rb
@@ -67,7 +67,7 @@ module AccountingReport
 
     def children
       family.children.select do |child|
-        child.age_group != :childcare && child.childcare_weeks.empty?
+        child.age_group == :childcare || child.childcare_weeks.empty?
       end
     end
   end
