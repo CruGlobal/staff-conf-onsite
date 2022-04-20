ea9ffe8f56aa12271c7a84900438db4d96146ed4
Jon Warmington <jonwarmington@japache.ca>
2022-04-20 13:32:07 -0400

    adjust selenium options

--- test/support/capybara.rb
+++ test/support/capybara.rb
@@ -7,11 +7,20 @@ end
 Capybara.register_driver :headless_chrome do |app|
   capabilities =
     Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu window-size=3840,21600) }
+      chromeOptions: { args: %w(headless disable-gpu disable-dev-shm-usage no-sandbox window-size=3840,21600 remote-debugging-port=9222) }
     )
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome,
-                                      desired_capabilities: capabilities)
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--remote-debugging-port=9515')
+  options.add_argument('--window-size=3840,21600')
+  
+  #Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara::Screenshot.register_driver(:headless_chrome) do |driver, path|
