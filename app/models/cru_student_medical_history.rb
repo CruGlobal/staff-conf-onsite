92e5dfb202b8082f5a6fdbfa9dd277073ab3c0b5
Jon Warmington <jonwarmington@japache.ca>
2022-05-09 11:12:59 -0400

    update behavioral issues to challenges:

--- app/models/cru_student_medical_history.rb
+++ app/models/cru_student_medical_history.rb
@@ -5,9 +5,9 @@ class CruStudentMedicalHistory < ApplicationRecord
   belongs_to :child
 
   multi_selection_attributes gtky_challenges: ['Death', 'Divorce', 'Abuse', 'Anger issues', 'Anxiety', 'Eating disorder',
-                                               'Major life change', 'Depression', 'Significant bullying', 'Behavioral issues',
+                                               'Major life change', 'Depression', 'Significant bullying', 'Behavioral challenges',
                                                'Self harm', 'Bipolar disorder', 'Foster Adoption', 'Other'],
-                             cs_health_misc: ['Developmental delay', 'Other special need', 'Behavioral issues', 'Disability',
+                             cs_health_misc: ['Developmental delay', 'Other special need', 'Behavioral challenges', 'Disability',
                                               'Extra assistance', 'Adaptive equipment', 'None of the above'], ## removed Sensory issues, added Other special need
                              cs_vip_comm: ['In simple phrases', 'In complete sentences', 'Other'],
                              cs_vip_stress: ['Noisy spaces', 'Crowded spaces', 'Loud noises', 'Other']
