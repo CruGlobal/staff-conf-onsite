1936d9d42be4cfc2ab26ffc1956b3b14ff9152c2
Brian Zoetewey <brian.zoetewey@cru.org>
2022-11-17 14:31:54 -0500

    Update for ECS multi-container. Remove newrelic.

--- lib/log/logger.rb
+++ lib/log/logger.rb
@@ -13,10 +13,10 @@ module Log
     end
 
     def create_formatter
-      if @readable
-        Log::Logger::FormatterReadable.new(STDOUT)
+      if ENV["AWS_EXECUTION_ENV"].present?
+        Log::Logger::Formatter.new(ENV["PROJECT_NAME"])
       else
-        Log::Logger::Formatter.new(ENV['PROJECT_NAME'])
+        Log::Logger::FormatterReadable.new($stdout)
       end
     end
   end
