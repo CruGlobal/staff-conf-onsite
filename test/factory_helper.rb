3b1aafe8e66095324265f6a156e6a86825ab8116
Jon Warmington <jonwarmington@japache.ca>
2023-05-12 11:40:43 -0400

    update all Faker attributes to meet latest version changes

--- test/factory_helper.rb
+++ test/factory_helper.rb
@@ -5,6 +5,7 @@ module FactoryHelper
 
   def random_future_date(start_date = nil)
     start_date ||= 1.year.from_now
-    Faker::Date.between(start_date, start_date + 2.years)
+    
+    Faker::Date.between(from: start_date, to: start_date + 2.years)
   end
 end
