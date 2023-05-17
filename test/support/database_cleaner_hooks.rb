1a1e3465bb0a8cad4fb25ebfb7b79982f216e9d4
Jon Warmington <jonwarmington@japache.ca>
2023-05-17 15:23:15 -0400

    Refactor for news version recomendations

--- test/support/database_cleaner_hooks.rb
+++ test/support/database_cleaner_hooks.rb
@@ -4,8 +4,14 @@ module Support
       #base.use_transactional_fixtures = false
 
       base.setup do
-        DatabaseCleaner.strategy = :truncation, { pre_count: true }
+        DatabaseCleaner.clean_with(:truncation, { pre_count: true })
+        DatabaseCleaner.strategy = :transaction
         DatabaseCleaner.start
+      end
+     
+
+      base.after do
+        DatabaseCleaner.clean
       end
 
       base.teardown { DatabaseCleaner.clean }
