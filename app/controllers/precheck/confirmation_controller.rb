198316766cf7a8431e5ad9e0353d4ed5c74188d0
N Kalampulan <nisam.kalampulan@japache.net>
2025-04-30 12:33:33 -0600

    fixing rubocop linting and syntax conventions

--- app/controllers/precheck/confirmation_controller.rb
+++ app/controllers/precheck/confirmation_controller.rb
@@ -3,7 +3,7 @@ class Precheck::ConfirmationController < PrecheckController
     redirect_to precheck_status_path and return unless Precheck::EligibilityService.new(family: @family).call
 
     hotel = params['hotel'] == 'Other' ? " OTHER - #{params['other_hotel']} " : params['hotel']
-    HotelStay.create(:hotel => hotel  , :family_id => @family.id)
+    HotelStay.create(hotel: hotel, family_id: @family.id)
  
     @family.update!(precheck_status: :approved)
     Precheck::UpdatedFamilyStatusService.new(family: @family).call
