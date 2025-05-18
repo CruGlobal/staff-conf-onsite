6e34ac570da7465559c465fc82aa193798cef8b9
N Kalampulan <nisam.kalampulan@japache.net>
2025-05-18 12:26:08 -0600

    trying to accept request from google script dsn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.hosts << /script\.google\.com/
+  config.hosts << /googleusercontent\.com/
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
