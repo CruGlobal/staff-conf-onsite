241313863f7669e46e5277fe537b059afd075c76
N Kalampulan <nisam.kalampulan@japache.net>
2025-05-20 12:25:10 -0600

    patch migration removed

--- db/migrate/20250426233016_add_medi_allergies_to_childcare_medical_histories.rb
+++ db/migrate/20250426233016_add_medi_allergies_to_childcare_medical_histories.rb
@@ -1,14 +1,25 @@
 class AddMediAllergiesToChildcareMedicalHistories < ActiveRecord::Migration[7.1]
   def change
-    add_column :childcare_medical_histories, :medi_allergy, :text, null: true
-    add_column :childcare_medical_histories, :allergies, :string, array: true, default: [], null: true
+    add_column :childcare_medical_histories, :cc_medi_allergy, :text, null: true
+    add_column :childcare_medical_histories, :cc_allergies, :string, array: true, default: [], null: true
     add_column :childcare_medical_histories, :non_immunizations, :string, array: true, default: [], null: true
-    add_column :childcare_medical_histories, :restriction_certified, :string, array: true, default: [], null: true    
-    add_column :childcare_medical_histories, :vip_developmental_age, :integer, null: true
-    add_column :childcare_medical_histories, :cc_vip_sitting, :text, null: true
+    add_column :childcare_medical_histories, :cc_restriction_certified, :string, array: true, default: [], null: true    
+    add_column :childcare_medical_histories, :cc_vip_sitting, :string, null: true
+    add_column :childcare_medical_histories, :cc_other_special_needs, :string, null: true
+    add_column :childcare_medical_histories, :cc_vip_staff_administered_meds, :string, null: true
+    add_column :childcare_medical_histories, :cc_vip_developmental_age, :string, null: true
 
-    add_column :cru_student_medical_histories, :med_allergy_multi, :text, null: true
+    
     add_column :cru_student_medical_histories, :crustu_forms_acknowledged, :string, array: true, default: [], null: true
+    add_column :cru_student_medical_histories, :cs_allergies, :string, array: true, default: [], null: true
+    add_column :cru_student_medical_histories, :cs_restriction_certified, :string, array: true, default: [], null: true        
+    add_column :cru_student_medical_histories, :cs_other_special_needs, :text, null: true
+    add_column :cru_student_medical_histories, :cs_vip_staff_administered_meds, :text, null: true
+    add_column :cru_student_medical_histories, :cs_chronic_health, :string, array: true, default: [], null: true    
+    add_column :cru_student_medical_histories, :cs_medications, :string, null: true   
+    add_column :cru_student_medical_histories, :cs_vip_developmental_age, :string, null: true
+    add_column :cru_student_medical_histories, :cs_restrictions, :string, null: true
+
     add_column :people, :tracking_id, :string, null: true
 
   end
